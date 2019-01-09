11dfe43dcaaf9be2bd368a7d1e0d18ce6ebd2ba7
arturbani <arturbani@gmail.com>
2019-01-09 09:13:37 -0200

    CSS adjusts + table rendered with json data

--- sorted-table/src/index.js
+++ sorted-table/src/index.js
@@ -3,7 +3,33 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import customData from './data.json';
 
+/* 
+
+  References used:
+    https://www.thesitewizard.com/css/make-table-cells-same-size.shtml
+    https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/sort
+    https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/Array/map
+
+*/
+
 class Table extends React.Component {
+  state = {
+    data: customData,
+    sortDirection: 'down', 
+    activeColumn: null, 
+  }
+
+  // sortColumn(column){
+  //   if (column === 'name')
+  //   if (column === 'address')
+  //   if (column === 'city')
+  //   if (column === 'region')
+  //   if (column === 'country')
+  //   if (column === 'birthday')
+
+
+  // }
+
   render(){
     return(
       <table>
@@ -30,7 +56,18 @@ class Table extends React.Component {
           </tr>
         </thead>
         <tbody>
-
+          {this.state.data.map((data) => {
+              return (
+                <tr>
+                  <td>{data.name}</td>
+                  <td>{data.address}</td>
+                  <td>{data.city}</td>
+                  <td>{data.region}</td>
+                  <td>{data.country}</td>
+                  <td>{data.birthday}</td>
+                </tr>
+              );
+          })}
         </tbody>
       </table>
     );
