b66a899e01ad0d1b3330814d1bc50c0224c0a372
arturbani <arturbani@gmail.com>
2019-01-09 07:58:49 -0200

    File organization

--- sorted-table/src/index.js
+++ sorted-table/src/index.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import customData from './data.json';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+class Table extends React.Component {
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+
+}
+
+// ========================================
+
+ReactDOM.render(
+  <Game />,
+  document.getElementById('root')
+);
