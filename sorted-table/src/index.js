7ccb6ade7d094bfe01c07c2c00d4532ebdfe0fb9
arturbani <arturbani@gmail.com>
2019-01-09 19:29:04 -0200

    Added some commentaries

--- sorted-table/src/index.js
+++ sorted-table/src/index.js
@@ -22,13 +22,12 @@ class Table extends React.Component {
         order: '>',
       }
     }
-
-    this.sortHandler = this.sortHandler.bind(this);
   }
 
   sortHandler(column){
     const order = this.state.sortSettings.order === '>' ? '<' : '>';
     const sortedData = this.state.data.sort((a, b) => { 
+      // DOBs are treated different because they don't need the toUpperCase() function, probably it would work the same though
       if (column === 'birthday'){
         
         // Explanation: If compareFunction(a, b) is less than 0, sort a to an index lower than b (i.e. a comes first).
