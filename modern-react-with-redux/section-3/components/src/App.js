c895b246db9d26a1716ea3ee103fdbc4ce9b59e3
arturbani <arturbani@gmail.com>
2019-01-22 11:35:24 -0200

    File reorganization

---
+++ modern-react-with-redux/section-3/components/src/App.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import faker from 'faker';
+import CommentDetail from './Components/CommentDetail/';
+import ApprovalCard from './Components/ApprovalCard/'
+
+class App extends React.Component {
+  render(){
+    return(
+      <div className="ui container comments">
+        <ApprovalCard>
+          <div align='center'>
+            <h4>Warning!</h4>
+            Are you sure you want to do this?
+          </div>
+        </ApprovalCard>
+
+        <ApprovalCard>
+        <CommentDetail
+          avatar = {faker.image.avatar()}
+          date = {'Today at 9:30 PM'}
+          author = {faker.name.firstName()}
+          text = {faker.lorem.sentence()} />
+        </ApprovalCard>
+
+        <ApprovalCard>
+          <CommentDetail
+          avatar = {faker.image.avatar()}
+          author = {faker.name.firstName()}
+          date = {'Today at 6:00 PM'}
+          text = {faker.lorem.sentence()} />
+        </ApprovalCard>
+
+        <ApprovalCard>
+          <CommentDetail
+          avatar = {faker.image.avatar()}
+          author = {faker.name.firstName()}
+          date = {'Yesterday at 2:00 AM'}
+          text = {faker.lorem.sentence()} />
+        </ApprovalCard>
+      </div>
+    );
+  };
+};
+
+export default App
--- modern-react-with-redux/section-3/components/src/index.js
+++ modern-react-with-redux/section-3/components/src/index.js
@@ -1,46 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import faker from 'faker';
-import CommentDetail from './Components/CommentDetail/';
-import ApprovalCard from './Components/ApprovalCard/'
+import App from './App.js';
 
-class App extends React.Component {
-  render(){
-    return(
-      <div className="ui container comments">
-        <ApprovalCard>
-          <div align='center'>
-            <h4>Warning!</h4>
-            Are you sure you want to do this?
-          </div>
-        </ApprovalCard>
-
-        <ApprovalCard>
-        <CommentDetail
-          avatar = {faker.image.avatar()}
-          date = {'Today at 9:30 PM'}
-          author = {faker.name.firstName()}
-          text = {faker.lorem.sentence()} />
-        </ApprovalCard>
-
-        <ApprovalCard>
-          <CommentDetail
-          avatar = {faker.image.avatar()}
-          author = {faker.name.firstName()}
-          date = {'Today at 6:00 PM'}
-          text = {faker.lorem.sentence()} />
-        </ApprovalCard>
-
-        <ApprovalCard>
-          <CommentDetail
-          avatar = {faker.image.avatar()}
-          author = {faker.name.firstName()}
-          date = {'Yesterday at 2:00 AM'}
-          text = {faker.lorem.sentence()} />
-        </ApprovalCard>
-      </div>
-    );
-  };
-};
-
-ReactDOM.render(<App />, document.querySelector('#root'));
+ReactDOM.render(
+  <App />,
+  document.querySelector('#root')
+);
