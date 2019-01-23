6ff7fdc506227faa2bbab84639a606fdcc481f53
arturbani <arturbani@gmail.com>
2019-01-23 11:44:24 -0200

    finished section-6 pratice

--- modern-react-with-redux/seasons/src/index.js
+++ modern-react-with-redux/seasons/src/index.js
@@ -1,50 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
-class App extends React.Component {
-  constructor(props){
-    super(props);
-
-    this.state = {
-      latitude: null,
-      errorMessage: '',
-    }
-
-    window.navigator.geolocation.getCurrentPosition(
-      position => {
-        this.setState({
-          latitude: position.coords.latitude
-        });
-      },
-      err => {
-        this.setState({
-          errorMessage: err.message
-        });
-      }
-    );
-  }
-
-  render(){
-    if (this.state.errorMessage && !this.state.latitude)
-      return(
-        <div>
-          Error: {this.state.errorMessage}
-        </div>
-      );
-
-    if (!this.state.errorMessage && this.state.latitude)
-        return(
-          <div>
-            Latitude: {this.state.latitude}
-          </div>
-        );
-
-      return <div>Loading!</div>;
-  }
-}
+import App from './App'
 
 ReactDOM.render(
   <App />,
   document.querySelector('#root')
 );
-
