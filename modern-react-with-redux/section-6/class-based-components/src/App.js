9431eb21ed80b575e570ec69aa4b7f0b7ff1e13d
arturbani <arturbani@gmail.com>
2019-01-23 12:09:29 -0200

    finished section 6

---
+++ modern-react-with-redux/section-6/class-based-components/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class App extends React.Component {
+  render() {
+    return(
+      <form>
+        <label>Enter a username:</label>
+        <input />
+      </form>
+    );
+  }
+}
+
+export default App
