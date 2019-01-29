ca3f6e21e2d6ab44dce98c71df052f21d3e59092
arturbani <arturbani@gmail.com>
2019-01-29 12:09:11 -0200

    finished section 15

--- modern-react-with-redux/redux/blog/src/Actions/index.js
+++ modern-react-with-redux/redux/blog/src/Actions/index.js
@@ -1,6 +1,12 @@
 import _ from "lodash";
 import jsonPlaceholder from "../API/jsonPlaceholder";
 
+export const fetchPostsAndUsers = () => async (dispatch, getState) => {
+  await dispatch(fetchPosts());
+  const userIds = _.uniq(_.map(getState().posts, "userId"));
+  userIds.forEach(id => dispatch(fetchUser(id)));
+};
+
 export const fetchPosts = () => async dispatch => {
   const response = await jsonPlaceholder.get("/posts");
 
@@ -10,24 +16,11 @@ export const fetchPosts = () => async dispatch => {
   });
 };
 
-// export const fetchUser  = id => async dispatch => {
-//   const response = await jsonPlaceholder.get(`/users/${id}`);
-
-//   dispatch({
-//     type: "FETCH_USER",
-//     payload: response.data
-//   });
-// };
-
-export const fetchUser = id => dispatch => {
-  _fetchUser(id, dispatch);
-};
-
-const _fetchUser = _.memoize(async (id, dispatch) => {
+export const fetchUser = id => async dispatch => {
   const response = await jsonPlaceholder.get(`/users/${id}`);
 
   dispatch({
     type: "FETCH_USER",
     payload: response.data
   });
-});
+};
--- modern-react-with-redux/redux/blog/src/Components/PostList.js
+++ modern-react-with-redux/redux/blog/src/Components/PostList.js
@@ -1,11 +1,11 @@
 import React from "react";
 import { connect } from "react-redux";
-import { fetchPosts } from "../Actions";
+import { fetchPostsAndUsers } from "../Actions";
 import UserHeader from "./UserHeader";
 
 class PostList extends React.Component {
   componentDidMount() {
-    this.props.fetchPosts();
+    this.props.fetchPostsAndUsers();
   }
 
   renderList() {
@@ -36,5 +36,5 @@ const mapStateToProps = state => {
 
 export default connect(
   mapStateToProps,
-  { fetchPosts }
+  { fetchPostsAndUsers }
 )(PostList);
