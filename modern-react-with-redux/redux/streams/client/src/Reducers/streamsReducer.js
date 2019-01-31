dd0a5a8fdae32248d9552f8f03370a2a0e1f5d66
arturbani <arturbani@gmail.com>
2019-01-31 17:02:01 -0200

    finished section 20

---
+++ modern-react-with-redux/redux/streams/client/src/Reducers/streamsReducer.js
@@ -0,0 +1,30 @@
+import _ from "lodash";
+import {
+  FETCH_STREAMS,
+  FETCH_STREAM,
+  DELETE_STREAM,
+  CREATE_STREAM,
+  EDIT_STREAM
+} from "../Actions/types";
+
+export default (state = {}, action) => {
+  switch (action.type) {
+    case FETCH_STREAMS:
+      return { ...state, ..._.mapKeys(action.payload, "id") };
+
+    case FETCH_STREAM:
+      return { ...state, [action.payload.id]: action.payload };
+
+    case DELETE_STREAM:
+      return _.omit(state, action.payload);
+
+    case CREATE_STREAM:
+      return { ...state, [action.payload.id]: action.payload };
+
+    case EDIT_STREAM:
+      return { ...state, [action.payload.id]: action.payload };
+
+    default:
+      return state;
+  }
+};
