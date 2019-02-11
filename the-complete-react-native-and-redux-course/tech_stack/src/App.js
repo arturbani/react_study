c8ac348044d5e2387471f5455f05cd58b20638c0
arturbani <arturbani@gmail.com>
2019-02-11 16:56:28 -0200

    finished section 13

---
+++ the-complete-react-native-and-redux-course/tech_stack/src/App.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { View } from "react-native";
+import { Provider } from "react-redux";
+import { createStore } from "redux";
+
+import Reducers from "./Reducers";
+import { Header } from "./Components/Common";
+import LibraryList from "./Components/LibraryList";
+
+const App = () => {
+  return (
+    <Provider store={createStore(Reducers)}>
+      <View>
+        <Header headerText="Tech Stack" />
+        <LibraryList />
+      </View>
+    </Provider>
+  );
+};
+
+export default App;
